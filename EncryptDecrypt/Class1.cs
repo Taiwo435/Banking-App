9b881059c8d721b8627992a12eaad945a4c909e1
Alan Escudero <alanescudero246@gmail.com>
2026-04-27 16:29:32 -0700

    Add EncryptDecrypt module

---
+++ EncryptDecrypt/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptDecrypt
+{
+    public class CryptClass
+    {
+        public static string HashPin(string pin)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(pin));
+                return Convert.ToBase64String(bytes);
+            }
+        }
+
+        public static bool VerifyPin(string inputPin, string storedHash)
+        {
+            return HashPin(inputPin) == storedHash;
+        }
+    }
+}
